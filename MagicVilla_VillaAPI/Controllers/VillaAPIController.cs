aca9322f16f9e6f61be1d804169428b7b867e148
Marian Simo <sim0302@vsb.cz>
2022-10-31 22:25:49 +0100

    min 47

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult <VillaDTO> GetVilla(int id)
+        {
+            return Ok(VillaStore.villaList.FirstOrDefault(u=>u.Id == id));
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CreatedDate { get; set; }
+    }
+}
